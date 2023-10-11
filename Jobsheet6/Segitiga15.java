899d334b4846fca45397c3afdee07c2413826133
isnaenytrilarassati <isnaenytrilarassati>
2023-10-11 16:21:47 +0700

    Percobaan

--- Jobsheet6/Segitiga15.java
+++ Jobsheet6/Segitiga15.java
@@ -15,16 +15,16 @@ public class Segitiga15{
 
         if (totalSudut == 180) {
             if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90)) {
-            System.out.println("Segitiga tersebut adalah segitiga siku - siku");  
+                System.out.println("Segitiga tersebut adalah segitiga siku - siku");  
              } else if (sudut1 == sudut2 && sudut2 == sudut3) {
-            System.out.println("Segitiga tersebut adalah segitiga sama sisi"); 
+                System.out.println("Segitiga tersebut adalah segitiga sama sisi"); 
              } else if ((sudut1 == sudut2 ) || (sudut2 == sudut3) || (sudut1 == sudut3)) {
                 System.out.println("Segitiga tersebut adalah segitiga sama kaki");
              } else if (sudut1 + sudut2 + sudut3 == 180){
                 System.out.println("Segitiga tersebut adalah segitiga sembarang");
              }
         }
-     else 
-        System.out.println ("Bukan Segitiga");
+        else 
+            System.out.println ("Bukan Segitiga");
     }
 }
