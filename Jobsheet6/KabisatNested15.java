e1fdf6a7cdbc9f55098c9838a514656a663f1028
isnaenytrilarassati <isnaenytrilarassati>
2023-10-11 11:12:53 +0700

    Modifikasi 1

--- Jobsheet6/KabisatNested15.java
+++ Jobsheet6/KabisatNested15.java
@@ -4,7 +4,6 @@ public class KabisatNested15 {
     public static void main(String[] args){
 
         Scanner sc = new Scanner(System.in);
-
         int tahun;
         System.out.println("Masukkan Tahun : ");
         tahun = sc.nextInt();
@@ -12,7 +11,9 @@ public class KabisatNested15 {
         if ((tahun % 4) == 0) {
             if ((tahun % 100) != 0) {
                 System.out.println("Tahun Kabisat");
-            }           
+            }  else {
+                System.out.println("Bukan Tahun Kabisat");
+            }
     } else
         System.out.println("Bukan Tahun Kabisat");
 }
