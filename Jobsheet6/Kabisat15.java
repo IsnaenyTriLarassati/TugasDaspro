87a1e0ed169eba5dce305537303b78454256f65d
isnaenytrilarassati <isnaenytrilarassati>
2023-10-11 12:03:06 +0700

    Modifikasi 1

---
+++ Jobsheet6/Kabisat15.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Kabisat15 {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        int tahun;
+        System.out.println("Masukkan Tahun :");
+        tahun = sc.nextInt();
+
+        if (( tahun % 4) == 0 && (tahun % 100) != 0 || ( tahun % 4) == 0 && (tahun % 100) != 0){
+            System.out.println("Tahun kabisat");
+        } else {
+            System.out.println("Bukan tahun kabisat");
+        }
+    }
+}
