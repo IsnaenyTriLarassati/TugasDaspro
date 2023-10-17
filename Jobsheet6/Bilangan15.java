9ddf29d868c2b10921935e011e20c7339f0c209c
isnaenytrilarassati <isnaenytrilarassati>
2023-10-17 08:19:37 +0700

    Modifikasi

--- Jobsheet6/Bilangan15.java
+++ Jobsheet6/Bilangan15.java
@@ -3,7 +3,7 @@ public class Bilangan15 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        int bil1, bil2, bil3;
+        int bil1, bil2, bil3, bilanganTerbesar;
 
         System.out.println("Masukkan Bilangan Pertama");
         bil1 = sc.nextInt();
@@ -11,19 +11,22 @@ public class Bilangan15 {
         bil2 = sc.nextInt();
         System.out.println("Masukkan Bilangan Ketiga");
         bil3 = sc.nextInt();
+        
 
         if (bil1>bil2) {
             if (bil1>bil3) {
-                System.out.println("Bilangan Terbesar = bil1");
+                System.out.println("Bilangan Terbesar = bilangan 1");
             } else {
-                System.out.println("Bilangan Terbesar = bil3");
+                System.out.println("Bilangan Terbesar = bilangan 3");
             }           
         } else {
             if(bil2>bil3) {
-                System.out.println("Bialngan Terbesar = bil2");
+                System.out.println("Bialngan Terbesar = bilangan 2");
             } else {
-                System.out.println("Bilangan Terbesar = bil 3");
+                System.out.println("Bilangan Terbesar = bilangan 3");
             }
         }
+        
+        bilanganTerbesar = bil1>bil2 ? (bil1 > bil2 ? bil1 : bil3) : (bil2 > bil3 ? bil2 : bil3);
     }
 }
