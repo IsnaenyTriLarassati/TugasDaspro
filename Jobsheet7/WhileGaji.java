c118071ab2b72d03b2afacaf7a6f2ec6d994a5b3
isnaenytrilarassati <isnaenytrilarassati>
2023-10-18 13:03:41 +0700

    Percobaan2

---
+++ Jobsheet7/WhileGaji.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class WhileGaji {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        int jumlahKaryawan, jumlahJamLembur;
+        double gajiLembur = 0,totalGajiLembur = 0;
+        String jabatan;
+
+        System.out.println("Masukkan jumlah karyawan: ");
+        jumlahKaryawan = sc.nextInt();
+
+        int i = 0;
+        while (i < jumlahKaryawan) {
+            System.out.println("Pilihan jabatan - Direktur, manajer, Karyawan");
+            System.out.print("Masukkan jabatan karyawan ke-" + (i+i) + ": ");
+            jabatan = sc.next();
+            System.out.print("Masukkan jumlah jam lembur: ");
+            jumlahJamLembur = sc.nextInt();
+            i++;
+
+            if (jabatan.equalsIgnoreCase("direktur")) {
+                continue;
+            } else if (jabatan.equalsIgnoreCase("manajer")){
+                gajiLembur = jumlahJamLembur * 1000000;
+            } else if (jabatan.equalsIgnoreCase("karyawan")) {
+                gajiLembur = jumlahJamLembur * 75000;
+            }
+
+            totalGajiLembur += gajiLembur;
+           System.out.println("Total gaji lembur: " + totalGajiLembur); 
+        }
+    }
+    
+}
