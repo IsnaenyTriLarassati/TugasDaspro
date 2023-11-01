d4842f22995980440b3b522f93569ebd945409f4
isnaenytrilarassati <isnaenytrilarassati>
2023-11-01 15:32:55 +0700

    Modifikasi1

--- Jobsheet9/LinearSearch15.java
+++ Jobsheet9/LinearSearch15.java
@@ -1,17 +1,31 @@
+import java.util.Scanner;
+
 public class LinearSearch15 {
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
         
-        int [] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
-        int key = 20;
-        int hasil = 0;
+        int[] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
+        int key, jumlahElemen;
+        int hasil = -1;
+
+        System.out.println("Masukkan jumlah elemen array: ");
+        jumlahElemen = sc.nextInt();
 
         for (int i = 0; i < arrayInt.length; i++) {
-            if (arrayInt[i] == key) {
-                hasil = i;
-                break;
-            }
+            System.out.println("Masukkan elemen array ke-" + i + ": ");
+            arrayInt[i] = sc.nextInt();
         }
 
-        System.out.println("Key ada dalam array pada posisi indeks ke-" + hasil);
+        System.out.println("Masukkan ley yang ingin dicari: ");
+        key = sc.nextInt();
+
+            for (int i = 0; i < jumlahElemen; i++) {
+                if (arrayInt[i] == key) {
+                    hasil = i;
+                    break;
+                }
+            }
+            System.out.println("Key ada di posisi indeks ke-" + hasil);
     }
 }
