98b8dd64235d90c0228778c67156dc2f57cad078
isnaenytrilarassati <isnaenytrilarassati>
2023-11-08 13:42:23 +0700

    Praktikum 2

---
+++ Jobsheet10/BioskopWithScanner15.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class BioskopWithScanner15 {
+    public static void main(String[] args) {
+       
+        int baris, kolom;
+        String nama, next;
+
+        Scanner sc = new Scanner (System.in);
+        String[][] penonton = new String[4][2];
+
+        while (true) {
+            System.out.print("Masukkan nama: ");
+            nama = sc.nextLine();
+            System.out.println("Masukkan baris: ");
+            baris = sc.nextInt();
+            System.out.println("Masukkan kolom: ");
+            kolom = sc.nextInt();
+            sc.nextLine();
+
+            penonton[baris-1][kolom-1] = nama;
+
+            System.out.println("Input penonton lainnya? (y/n): ");
+            next = sc.nextLine();
+
+            if (next.equalsIgnoreCase("n")) {
+                break;
+            }
+        }
+    }
+}
