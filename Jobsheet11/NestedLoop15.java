c7d99a30dbe6035fcdcbcab6af01ed121d63fc8c
isnaenytrilarassati <isnaenytrilarassati>
2023-11-15 14:37:41 +0700

    Percobaan 4 _modifikasi_

--- Jobsheet11/NestedLoop15.java
+++ Jobsheet11/NestedLoop15.java
@@ -4,7 +4,10 @@ public class NestedLoop15 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
 
-        double[][] suhu = new double[2][4];
+        System.out.print("Masukkan jumlah kota: ");
+        int jumlahKota = sc.nextInt();
+
+        double[][] suhu = new double[jumlahKota][4];
 
         for (int i = 0; i < suhu.length; i++) {
             System.out.println("KOTA KE-" + (i + 1));
@@ -17,12 +20,17 @@ public class NestedLoop15 {
 
         for (int i = 0; i < suhu.length; i++) {
             System.out.print("KOTA KE-" + (i + 1) + ": ");
+            double sum = 0.0;
 
-            for (int j = 0; j < suhu[i].length; j++) {
-                System.out.print(suhu[i][j] + " ");
+            for (double temp : suhu[i]) {
+                System.out.print(temp + " ");
+                sum += temp;
             }
 
-            System.out.println();
+            sum /= suhu[i].length;
+            System.out.println("\nRata-rata: " + sum);
         }
     }  
 }
+
+
