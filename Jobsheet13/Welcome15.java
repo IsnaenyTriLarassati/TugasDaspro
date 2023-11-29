81aaae7a2ab5fc63dc48c9b0fdd17d209be14df7
isnaenytrilarassati <isnaenytrilarassati>
2023-11-29 10:51:55 +0700

    Fungsi Tanpa Parameter

---
+++ Jobsheet13/Welcome15.java
@@ -0,0 +1,9 @@
+public class Welcome15 {
+
+    public static void displayWelcomeMessafe(){
+        System.out.println("Selamat dtang di Politeknik Negeri Malang");
+    }
+    public static void main(String[] args) {
+        displayWelcomeMessafe();
+    }
+}
