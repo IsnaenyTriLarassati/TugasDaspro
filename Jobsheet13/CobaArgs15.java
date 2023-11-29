b3049d5e25e12a3f4074747c021039cd7c7ef402
isnaenytrilarassati <isnaenytrilarassati>
2023-11-29 13:52:25 +0700

    Memahami args pada fungsi main() 1

---
+++ Jobsheet13/CobaArgs15.java
@@ -0,0 +1,14 @@
+public class CobaArgs15 {
+    public static void main(String[] args) {
+
+        // System.out.println("Hello world!");
+
+        // for (int i = 0; i < args.length; i++) {
+        //     System.out.println(args[i]);
+        // }
+        
+        // modifikasi
+        System.out.println("Hai " + args[0]);
+        System.out.println("Selamat datang di " + args[1]);
+    }
+}
