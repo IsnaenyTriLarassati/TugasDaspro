e3bec5a96859de612e5b9f8d8a43860388252f82
isnaenytrilarassati <isnaenytrilarassati>
2023-11-29 13:25:38 +0700

    Manfaat FUngsi 2

--- Jobsheet13/School15.java
+++ Jobsheet13/School15.java
@@ -6,50 +6,12 @@ public class School15 {
     String[] students1B = { "Bobby", "Brian", "Bill" };
     String[] students1C = { "Clara", "Celine", "Charlie", "Caroline" };
     String[] students1D = { "Daniel", "Daisy", "Diana" };
+    
+    display(students1A);
+    display(students1B);
+    display(students1C);
+    display(students1D);
 
-    // daftar array students1A
-    System.out.printf("=========================%n");
-    System.out.printf("| %-3s | %-15s | %n", "NO", "NAME");
-    System.out.printf("=========================%n");
-
-    for (int i = 0; i < students1A.length; i++) {
-        System.out.printf("| %-3s | %-15s | %n", (i + 1), students1A[i]);
-    }
-
-    System.out.printf("=========================%n%n");
-
-    // daftar array students1B
-    System.out.printf("=========================%n");
-    System.out.printf("| %-3s | %-15s | %n", "NO", "NAME");
-    System.out.printf("=========================%n");
-
-    for (int i = 0; i < students1B.length; i++) {
-        System.out.printf("| %-3s | %-15s | %n", (i + 1), students1B[i]);
-    }
-
-    System.out.printf("=========================%n%n");
-
-    // daftar array students1C
-    System.out.printf("=========================%n");
-    System.out.printf("| %-3s | %-15s | %n", "NO", "NAME");
-    System.out.printf("=========================%n");
-
-    for (int i = 0; i < students1C.length; i++) {
-        System.out.printf("| %-3s | %-15s | %n", (i + 1), students1C[i]);
-    }
-
-    System.out.printf("=========================%n%n");
-
-    // daftar array students1D
-    System.out.printf("=========================%n");
-    System.out.printf("| %-3s | %-15s | %n", "NO", "NAME");
-    System.out.printf("=========================%n");
-
-    for (int i = 0; i < students1D.length; i++) {
-        System.out.printf("| %-3s | %-15s | %n", (i + 1), students1D[i]);
-    }
-
-    System.out.printf("=========================%n%n");
     }
 
     public static void display(String[] students) {
